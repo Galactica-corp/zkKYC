17174610de0bd12b8514789c823126b1e9292a35
anhdungle <anhdungle634@gmail.com>
2022-10-14 11:55:56 +0200

    add second test for merkle proof

---
+++ test/circuits/merkleProof2.ts
@@ -0,0 +1,77 @@
+import { assert, expect } from 'chai';
+import { readFileSync } from 'fs';
+import hre from 'hardhat';
+import { CircuitTestUtils } from 'hardhat-circom';
+
+describe('Merkle Proof 2 Circuit Component', () => {
+  let circuit: CircuitTestUtils;
+
+  const sampleInput = JSON.parse(
+    readFileSync('./circuits/input/merkleProof_2.json', 'utf8')
+  );
+  const expectedRoot =
+    '13279458920664938636312831934678353064136952143172930344736897756651772527484';
+
+  const sanityCheck = true;
+
+  before(async () => {
+    circuit = await hre.circuitTest.setup('merkleProof2');
+  });
+
+  it('produces a witness with valid constraints', async () => {
+    const witness = await circuit.calculateWitness(sampleInput, sanityCheck);
+    await circuit.checkConstraints(witness);
+  });
+
+  it('has expected witness values', async () => {
+    const witness = await circuit.calculateLabeledWitness(
+      sampleInput,
+      sanityCheck
+    );
+
+    assert.propertyVal(witness, 'main.leaf', sampleInput.leaf);
+    assert.propertyVal(
+      witness,
+      'main.pathIndices',
+      BigInt(sampleInput.pathIndices).toString()
+    );
+    // check resulting root as output
+    assert.propertyVal(witness, 'main.root', expectedRoot);
+  });
+
+  it('has the correct root as output', async () => {
+    const expected = { root: expectedRoot };
+    const witness = await circuit.calculateWitness(sampleInput, sanityCheck);
+    await circuit.assertOut(witness, expected);
+  });
+
+  it('output changes on having a different leaf', async () => {
+    let forgedInput = sampleInput;
+    forgedInput.leaf += 1;
+    const witness = await circuit.calculateLabeledWitness(
+      forgedInput,
+      sanityCheck
+    );
+    assert.notPropertyVal(witness, 'main.root', expectedRoot);
+  });
+
+  it('output changes on having a different path', async () => {
+    let forgedInput = sampleInput;
+    forgedInput.pathIndices -= 1; // flip some bits to have a different path
+    const witness = await circuit.calculateLabeledWitness(
+      forgedInput,
+      sanityCheck
+    );
+    assert.notPropertyVal(witness, 'main.root', expectedRoot);
+  });
+
+  it('output changes on having a different neighbor hashes', async () => {
+    let forgedInput = sampleInput;
+    forgedInput.pathElements[1] = forgedInput.pathElements[2];
+    const witness = await circuit.calculateLabeledWitness(
+      forgedInput,
+      sanityCheck
+    );
+    assert.notPropertyVal(witness, 'main.root', expectedRoot);
+  });
+});
