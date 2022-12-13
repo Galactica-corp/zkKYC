16376d6f65e18538ebb85a397b04998f3dc947b2
anhdungle <anhdungle634@gmail.com>
2022-12-14 03:17:40 +0700

    resolve conflict in ageProofZkKYC.json

--- test/contracts/KYCRecordRegistry.ts
+++ test/contracts/KYCRecordRegistry.ts
@@ -2,50 +2,70 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { buildEddsa } from 'circomlibjs';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
 import { MerkleTree } from '../../lib/merkleTree';
-import { overwriteArtifact, fromDecToHex, fromHexToBytes32, generateRandomBytes32Array, arrayToBigInt } from '../../lib/helpers';
+const hre = require('hardhat');
+import {
+  overwriteArtifact,
+  fromDecToHex,
+  fromHexToBytes32,
+  generateRandomBytes32Array,
+  arrayToBigInt,
+} from '../../lib/helpers';
 import { poseidonContract } from 'circomlibjs';
 
 
 describe('KYCRecordRegistry', () => {
+  let deployer: SignerWithAddress;
+
+  beforeEach(async () => {
+    [deployer] = await hre.ethers.getSigners();
+  });
   /**
    * Deploy fixtures
    *
    * @returns fixtures
    */
   async function deploy() {
-    await overwriteArtifact(
-        hre,
-        'PoseidonT3',
-        poseidonContract.createCode(2),
-    );
+    await overwriteArtifact(hre, 'PoseidonT3', poseidonContract.createCode(2));
 
     const PoseidonT3 = await ethers.getContractFactory('PoseidonT3');
     const poseidonT3 = await PoseidonT3.deploy();
 
-    const KYCRecordRegistryTest = await ethers.getContractFactory('KYCRecordRegistryTest', {
-      libraries: {
-        PoseidonT3: poseidonT3.address,
-      },
-    });
-    const KYCRecordRegistry = await KYCRecordRegistryTest.deploy();
+    const KYCCenterRegistryFactory = await ethers.getContractFactory(
+      'KYCCenterRegistry'
+    );
+    const KYCCenterRegistry = await KYCCenterRegistryFactory.deploy();
+
+    const KYCRecordRegistryTest = await ethers.getContractFactory(
+      'KYCRecordRegistryTest',
+      {
+        libraries: {
+          PoseidonT3: poseidonT3.address,
+        },
+      }
+    );
+    const KYCRecordRegistry = await KYCRecordRegistryTest.deploy(
+      KYCCenterRegistry.address
+    );
 
     return {
       KYCRecordRegistry,
+      KYCCenterRegistry,
     };
   }
 
   it("Shouldn't initialize twice", async () => {
-    const { KYCRecordRegistry } = await loadFixture(deploy);
+    const { KYCRecordRegistry, KYCCenterRegistry } = await loadFixture(deploy);
 
-    await expect(KYCRecordRegistry.doubleInit()).to.be.revertedWith(
-      'Initializable: contract is not initializing',
-    );
+    await expect(
+      KYCRecordRegistry.doubleInit(KYCCenterRegistry.address)
+    ).to.be.revertedWith('Initializable: contract is not initializing');
   });
 
   it('Should calculate zero values', async () => {
-    const { KYCRecordRegistry } = await loadFixture(deploy);
+    const { KYCRecordRegistry, KYCCenterRegistry } = await loadFixture(deploy);
 
     const eddsa = await buildEddsa();
     const treeDepth = 32;
@@ -53,28 +73,32 @@ describe('KYCRecordRegistry', () => {
 
     // Each value in the zero values array should be the same
     for (let i = 0; i < treeDepth; i++) {
-        expect(await KYCRecordRegistry.zeros(i)).to.equal(fromHexToBytes32(fromDecToHex(merkleTree.emptyBranchLevels[i])));
+      expect(await KYCRecordRegistry.zeros(i)).to.equal(
+        fromHexToBytes32(fromDecToHex(merkleTree.emptyBranchLevels[i]))
+      );
     }
-
   });
 
   it('Should calculate empty root', async () => {
-    const { KYCRecordRegistry } = await loadFixture(deploy);
+    const { KYCRecordRegistry, KYCCenterRegistry } = await loadFixture(deploy);
 
     const eddsa = await buildEddsa();
     const treeDepth = 32;
     const merkleTree = new MerkleTree(treeDepth, eddsa.poseidon);
 
     // Should initialize empty root correctly
-    expect(await KYCRecordRegistry.merkleRoot()).to.equal(fromHexToBytes32(fromDecToHex(merkleTree.root)));
+    expect(await KYCRecordRegistry.merkleRoot()).to.equal(
+      fromHexToBytes32(fromDecToHex(merkleTree.root))
+    );
   });
-
 
   it('Should incrementally insert elements', async function () {
     let loops = 5;
 
+    const { KYCRecordRegistry, KYCCenterRegistry } = await loadFixture(deploy);
 
-    const { KYCRecordRegistry } = await loadFixture(deploy);
+    // add deployer as a KYCCenter
+    KYCCenterRegistry.grantKYCCenterRole(deployer.address);
 
     const eddsa = await buildEddsa();
     const treeDepth = 32;
@@ -84,7 +108,9 @@ describe('KYCRecordRegistry', () => {
     for (let i = 0; i < loops; i += 1) {
       // Check the insertion numbers
       expect(
-        await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(insertList.length),
+        await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(
+          insertList.length
+        )
       ).to.deep.equal([0, merkleTree.tree[0].length]);
 
       // Update with insert list on local and contract
@@ -92,19 +118,29 @@ describe('KYCRecordRegistry', () => {
       merkleTree.insertleaves(insertList);
 
       // Check roots match
-      expect(await KYCRecordRegistry.merkleRoot()).to.equal(fromHexToBytes32(fromDecToHex(merkleTree.root)));
+      expect(await KYCRecordRegistry.merkleRoot()).to.equal(
+        fromHexToBytes32(fromDecToHex(merkleTree.root))
+      );
 
       // Check tree length matches
-      expect(await KYCRecordRegistry.nextLeafIndex()).to.equal(merkleTree.tree[0].length);
+      expect(await KYCRecordRegistry.nextLeafIndex()).to.equal(
+        merkleTree.tree[0].length
+      );
 
       // Add another element to insert list
-      insertList.push(fromHexToBytes32(arrayToBigInt(generateRandomBytes32Array(1)[0]).toString(16)));
+      insertList.push(
+        fromHexToBytes32(
+          arrayToBigInt(generateRandomBytes32Array(1)[0]).toString(16)
+        )
+      );
     }
   });
 
   it('Should roll over to new tree', async function () {
-    const { KYCRecordRegistry } = await loadFixture(deploy);
+    const { KYCRecordRegistry, KYCCenterRegistry } = await loadFixture(deploy);
 
+    // add deployer as a KYCCenter
+    KYCCenterRegistry.grantKYCCenterRole(deployer.address);
     // Check tree number is 0
     expect(await KYCRecordRegistry.treeNumber()).to.equal(0);
 
@@ -113,19 +149,17 @@ describe('KYCRecordRegistry', () => {
     await KYCRecordRegistry.setNextLeafIndex(2 ** treeDepth - 2);
 
     // Check the insertion numbers
-    expect(await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(1)).to.deep.equal([
-      0,
-      2 ** treeDepth - 2,
-    ]);
+    expect(
+      await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(1)
+    ).to.deep.equal([0, 2 ** treeDepth - 2]);
 
     // Insert leaf hash
     await KYCRecordRegistry.insertLeavesTest(generateRandomBytes32Array(1));
 
     // Check the insertion numbers
-    expect(await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(1)).to.deep.equal([
-      0,
-      2 ** treeDepth - 1,
-    ]);
+    expect(
+      await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(1)
+    ).to.deep.equal([0, 2 ** treeDepth - 1]);
 
     // Check tree number is 0
     expect(await KYCRecordRegistry.treeNumber()).to.equal(0);
@@ -134,12 +168,33 @@ describe('KYCRecordRegistry', () => {
     await KYCRecordRegistry.insertLeavesTest(generateRandomBytes32Array(1));
 
     // Check the insertion numbers
-    expect(await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(1)).to.deep.equal([1, 0]);
+    expect(
+      await KYCRecordRegistry.getInsertionTreeNumberAndStartingIndex(1)
+    ).to.deep.equal([1, 0]);
 
     // Insert leaf hash
     await KYCRecordRegistry.insertLeavesTest(generateRandomBytes32Array(1));
 
     // Check tree number is 1
     expect(await KYCRecordRegistry.treeNumber()).to.equal(1);
+  });
+
+  it('Only KYC Center can add leaf', async function () {
+    const { KYCRecordRegistry, KYCCenterRegistry } = await loadFixture(deploy);
+
+    const leafHash = fromHexToBytes32(
+      arrayToBigInt(generateRandomBytes32Array(1)[0]).toString(16)
+    );
+
+    await expect(
+      KYCRecordRegistry.connect(deployer).addZkKYCRecord(leafHash)
+    ).to.be.revertedWith('KYCRecordRegistry: not a KYC Center');
+
+    // add deployer as a KYCCenter
+    KYCCenterRegistry.grantKYCCenterRole(deployer.address);
+
+    await expect(KYCRecordRegistry.connect(deployer).addZkKYCRecord(leafHash))
+      .to.emit(KYCRecordRegistry, 'zkKYCRecordAddition')
+      .withArgs(leafHash, deployer.address);
   });
 });
