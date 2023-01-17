25daf880d3d52929f71f00939be95e5f9e3ccc9d
anhdungle <anhdungle634@gmail.com>
2023-01-17 07:48:09 +0100

    add onchain data to store user encrypted data

--- contracts/UserEncryptedData.sol
+++ contracts/UserEncryptedData.sol
@@ -5,11 +5,16 @@ pragma solidity ^0.8.9;
 
 contract UserEncryptedData {
 
+    // mapping to store user encrypted data
+    mapping(address => bytes) public encryptedData;
+
+
     // event emitted when user data is submitted
     event AddEncryptedData(address indexed user, bytes data);
 
     // function to add user encrypted data as event
     function addEncryptedData(bytes calldata data) public {
+        encryptedData[msg.sender] = data;
         emit AddEncryptedData(msg.sender, data);
     }
 }
