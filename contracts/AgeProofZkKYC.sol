11b3ba4c155a027243fbe6262b18824cd0c2b945
anhdungle <anhdungle634@gmail.com>
2023-01-31 13:24:07 +0100

    add investigation data public inputs into AgeProofZkKYC wrapper contract

--- contracts/AgeProofZkKYC.sol
+++ contracts/AgeProofZkKYC.sol
@@ -5,15 +5,18 @@ import "./Ownable.sol";
 import "./interfaces/IAgeProofZkKYCVerifier.sol";
 import "./interfaces/IKYCRegistry.sol";
 import "./libraries/BokkyPooBahsDateTimeLibrary.sol";
+import "./interfaces/IGalacticaInstitution.sol";
 
 contract AgeProofZkKYC is Ownable{
     IAgeProofZkKYCVerifier public verifier;
     IKYCRegistry public KYCRegistry;
+    IGalacticaInstitution public galacticaInstitution;
     uint256 public constant timeDifferenceTolerance = 120; // the maximal difference between the onchain time and public input current time
 
-    constructor(address _owner, address _verifier, address _KYCRegistry) Ownable(_owner) public {
+    constructor(address _owner, address _verifier, address _KYCRegistry, address _galacticaInstitution) Ownable(_owner) public {
         verifier = IAgeProofZkKYCVerifier(_verifier);
         KYCRegistry = IKYCRegistry(_KYCRegistry);
+        galacticaInstitution = IGalacticaInstitution(_galacticaInstitution);
     }
 
     function setVerifier(IAgeProofZkKYCVerifier newVerifier) public onlyOwner {
@@ -24,13 +27,17 @@ contract AgeProofZkKYC is Ownable{
         KYCRegistry = newKYCRegistry;
     }
 
+    function setGalacticaInstituion(IGalacticaInstitution newGalacticaInstitution) public onlyOwner {
+        galacticaInstitution = newGalacticaInstitution;
+    }
+
     //a, b, c are the proof
     // input array contains the public parameters: isValid, root, currentTime, currentYear, currentMonth, currentDay, ageThreshold
     function verifyProof(
             uint[2] memory a,
             uint[2][2] memory b,
             uint[2] memory c,
-            uint[8] memory input
+            uint[14] memory input
         ) public view {
         
         require(input[0] == 1, "the proof output is not valid");
@@ -54,6 +61,10 @@ contract AgeProofZkKYC is Ownable{
         require(onchainYear == input[4], "the current year is incorrect");
         require(onchainMonth == input[5], "the current month is incorrect");
         require(onchainDay == input[6], "the current day is incorrect");
+
+        // check that the institution public key corresponds to the onchain one;
+        require(galacticaInstitution.institutionPubKey(0) == input[9], "the first part of institution pubkey is incorrect");
+        require(galacticaInstitution.institutionPubKey(1) == input[10], "the second part of institution pubkey is incorrect");
 
         require(verifier.verifyProof(a, b, c, input), "the proof is incorrect");
     }
