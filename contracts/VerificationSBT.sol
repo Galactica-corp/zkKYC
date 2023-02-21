92db23bf77cc1c1337353f64359a22305937f0fa
anhdungle <anhdungle634@gmail.com>
2023-02-21 10:46:21 +0100

    clarify a comment

--- contracts/VerificationSBT.sol
+++ contracts/VerificationSBT.sol
@@ -27,7 +27,7 @@ contract VerificationSBT {
     event VerificationSBTMinted(address indexed dApp, address indexed user, bytes32 indexed humanID);
 
     function mintVerificationSBT(address user, IVerifierWrapper _verifierWrapper, uint _expirationTime, bytes32[2] calldata _encryptedData, uint256[2] calldata _userPubKey, bytes32 _humanID) public {
-        // The msg.sender is included in the key, so that each dApp can only mint SBTs corresponding to it
+        // The function is public so anyone can call it, but the msg.sender is included in the key, so that each dApp can only mint SBTs corresponding to it. When we search for relevant SBTs we only care about the corresponding mapping keys.
         VerificationSBTMapping[keccak256(abi.encode(user, msg.sender))] = VerificationSBTInfo({
             dApp: msg.sender,
             verifierWrapper: _verifierWrapper, 
