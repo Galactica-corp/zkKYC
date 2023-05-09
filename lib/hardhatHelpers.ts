450d6d84d38c6283d063839af0df1a50beef8997
Frederik Zwilling <frederik.zwilling@gmail.com>
2023-05-09 11:50:43 +0200

    GPLv3 lincense notices

--- lib/hardhatHelpers.ts
+++ lib/hardhatHelpers.ts
@@ -3,6 +3,7 @@ import { Contract, Signer } from "ethers";
 import { FactoryOptions } from "hardhat/types";
 import chalk from "chalk";
 
+/* Copyright (C) 2023 Galactica Network. This file is part of zkKYC. zkKYC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. zkKYC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
 /**
  * Helper function to deploy a smart contract and verify it on the block explorer
  * 
