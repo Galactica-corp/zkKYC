bfcb283ac1cc86723ae0c6a0f59c60234079881d
Frederik Zwilling <frederik.zwilling@gmail.com>
2023-07-06 18:31:30 +0200

    reenabled devnet guardian code

--- contracts/mock/DevnetGuardian.sol
+++ contracts/mock/DevnetGuardian.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.7;
 pragma abicoder v2;
 
-import { KYCRecordRegistry } from "../KYCRecordRegistry.sol";
+import {KYCRecordRegistry} from "../KYCRecordRegistry.sol";
 
 /**
  * @title DevnetGuardian is a simple KYC guardian that everyone can use to issue zkKYCs on Devnet
@@ -10,30 +10,31 @@ import { KYCRecordRegistry } from "../KYCRecordRegistry.sol";
  * @notice Works as interface to the KYCRecordRegistry providing a simple way to issue zkKYCs while being authorized as a guardian
  */
 contract DevnetGuardian {
-  KYCRecordRegistry public recordRegistry;
+    KYCRecordRegistry public recordRegistry;
 
-  constructor(address _recordRegistry) {
-    recordRegistry = KYCRecordRegistry(_recordRegistry);
-  }
+    constructor(address _recordRegistry) {
+        recordRegistry = KYCRecordRegistry(_recordRegistry);
+    }
 
-  // /**
-  //  * @notice addZkKYCRecord issues a zkKYC record by adding it to the Merkle tree
-  //  * @param zkKYCRecordLeafHash - hash of the zkKYC record leaf
-  //  */
-  // function addZkKYCRecord(bytes32 zkKYCRecordLeafHash) public {
-  //   recordRegistry.addZkKYCRecord(zkKYCRecordLeafHash);
-  // }
+    /**
+     * @notice addZkKYCRecord issues a zkKYC record by adding it to the Merkle tree
+     * @param zkKYCRecordLeafHash - hash of the zkKYC record leaf
+     */
+    function addZkKYCRecord(bytes32 zkKYCRecordLeafHash) public {
+        recordRegistry.addZkKYCRecord(zkKYCRecordLeafHash);
+    }
 
-  // /**
-  //  * @notice Gets tree number that new zkKYC record will get inserted to
-  //  * @param _newZkKYCRecords - number of new zkKYC records
-  //  * @return treeNumber, startingIndex
-  //  */
-  // function getInsertionTreeNumberAndStartingIndex(uint256 _newZkKYCRecords)
-  //   public
-  //   view
-  //   returns (uint256, uint256)
-  // {
-  //   return recordRegistry.getInsertionTreeNumberAndStartingIndex(_newZkKYCRecords);
-  // }
+    /**
+     * @notice Gets tree number that new zkKYC record will get inserted to
+     * @param _newZkKYCRecords - number of new zkKYC records
+     * @return treeNumber, startingIndex
+     */
+    function getInsertionTreeNumberAndStartingIndex(
+        uint256 _newZkKYCRecords
+    ) public view returns (uint256, uint256) {
+        return
+            recordRegistry.getInsertionTreeNumberAndStartingIndex(
+                _newZkKYCRecords
+            );
+    }
 }
